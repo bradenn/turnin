c5a6b68cfff32b40fc9b18a53583149ebb01003c
bradenn <braden@bradencnicholson.com>
2020-04-27 20:34:31 -0700

    BONK: Some bad promises were fixed.

--- routes/login.js
+++ routes/login.js
@@ -3,25 +3,16 @@ let User = require('../models/user');
 
 router.get('/', (req, res, next) => {
     if (req.user != null) res.redirect("/");
-    return res.render("login", {
-        title: "Login",
-        user: req.user
-    });
+    return res.render("login");
 });
 
 router.post('/', async (req, res, next) => {
     if (req.body.logUser && req.body.logPassword) {
-        await User.authenticate(req.body.logUser, req.body.logPassword).then((data) => {
-            if(data.err){
-                return res.render("login", {
-                    err: data.err
-                });
-            }else{
-                req.session.userId = data.user._id;
-                return res.redirect('/');
-            }
+        User.authenticate(req.body.logUser, req.body.logPassword).then(user => {
+            req.session.userId = user._id;
+            return res.redirect('/');
         }).catch((error) => {
-            console.log(error);
+            return res.render("login", {err: error});
         });
     }
 });
