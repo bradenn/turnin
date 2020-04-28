f2c5ca644c856fe84fca9d117f6b1db947cddb57
bradenn <braden@bradencnicholson.com>
2020-04-27 21:15:21 -0700

    Lots of fun fixes to docker and env

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t turnin-gateway .
-docker tag turnin-gateway bradenn/turnin-gateway:1.0.0
-docker run -p 0.0.0.0:3000:3000/tcp bradenn/turnin-gateway:1.0.0
+docker tag turnin-gateway bradenn/turnin-gateway:1.0.2
+docker run -p 0.0.0.0:3000:3000/tcp bradenn/turnin-gateway:1.0.2
